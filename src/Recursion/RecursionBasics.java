d6a36db17ca4dcf5427012ca06e7650bc4990df7
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-04-29 06:15:28 +0530

    factorial, binary search, sum upto n numbers, sum of digits, multiplication of digits, reverse number

--- src/Recursion/RecursionBasics.java
+++ src/Recursion/RecursionBasics.java
@@ -52,6 +52,8 @@ public class RecursionBasics {
         return nthFib;
     }
 
+    //We need to take start and end value in the function arguments as these values are changing in every call and are
+    //responsible for shortening the call stack
     public static boolean binarySearchUsingRecursion(int[] arr,int start,int end, int searchValue){
 
         int mid = (start+end)/2;
@@ -77,8 +79,88 @@ public class RecursionBasics {
         boolean finallyFound = foundInLeft || foundInRight;
 
         return finallyFound;
-
     }
+
+    public static int factorial(int n){
+        if(n==0){
+            return 1;
+        }
+        int nMinusOneThFactorial = factorial(n-1);
+
+        //Answer of nth factorial is, n times of (n-1)th factorial
+        return n*nMinusOneThFactorial;
+    }
+
+    static int search(int[] arr, int target, int s, int e) {
+        if (s > e) {
+            return -1;
+        }
+        int m = s + (e - s) / 2;
+        if (arr[m] == target) {
+            return m;
+        }
+        if (target < arr[m]) {
+            return search(arr, target, s, m - 1);
+        }
+        return search(arr, target, m + 1, e);
+    }
+
+    static int sumOntoNthNumber(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int sumOntoNMinusOneThNumber = sumOntoNthNumber(n-1);
+        return n+sumOntoNMinusOneThNumber;
+    }
+
+    static int sumOfDigits(int n){
+
+        if(n==0){
+            return 0;
+        }
+
+        int currentDigit = n%10;
+        int sumOfRemainingNumber = sumOfDigits(n/10);
+
+        int finalAns = currentDigit + sumOfRemainingNumber;
+
+        return finalAns;
+    }
+
+    static int productOfDigits(int n){
+
+        if((n%10) == n){
+            return n;
+        }
+
+        int currentDigit = n%10;
+        int sumOfRemainingNumber = productOfDigits(n/10);
+
+        int finalAns = currentDigit * sumOfRemainingNumber;
+
+        return finalAns;
+    }
+
+    public static int reverseANumber(int n){
+        if(n%10==n){
+            return n;
+        }
+
+        int extractedDigit = n%10;
+        int reverseOfRemainingNumber = reverseANumber(n/10);
+
+        int number = reverseOfRemainingNumber;
+        int digit_count = 0;
+        while (number!=0){
+            digit_count++;
+            number= number/10;
+        }
+        int reversedNumber = (int) (extractedDigit*Math.pow(10,digit_count) + reverseOfRemainingNumber);
+
+        return reversedNumber;
+    }
+
+    //f(n--) wont work, f(--n) works
 
     public static void main(String[] args) {
         printFromNtoX(3,10);
@@ -90,6 +172,11 @@ public class RecursionBasics {
         System.out.println(NthFibonacciNumber(8));
         int[] arr = new int[]{5,6,7,12};
         System.out.println(binarySearchUsingRecursion(arr,0, arr.length-1, 4));
+        System.out.println(factorial(5));
+        System.out.println(sumOntoNthNumber(4));
+        System.out.println(sumOfDigits(2543));
+        System.out.println(productOfDigits(2543));
+        System.out.println(reverseANumber(175678));
     }
 
 }
