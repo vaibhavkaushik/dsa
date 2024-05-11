c736d99ae7fbbab513d2e5259ca167095a511831
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-12 01:25:36 +0530

    Leetcode 108

--- src/Trees/BinaryTree.java
+++ src/Trees/BinaryTree.java
@@ -30,4 +30,29 @@ public class BinaryTree {
 
         return leftAns && rightAns;
     }
+
+    //Leetcode 108
+    public TreeNode sortedArrayToBST(int[] nums) {
+        TreeNode ans = sortedArrayToBSTHelper(nums, null, 0, nums.length - 1);
+        return ans;
+    }
+
+    public TreeNode sortedArrayToBSTHelper(int[] nums, TreeNode currRoot, int start, int end) {
+
+        if (end < start) {
+            return null;
+        }
+
+        int mid = start + (end - start) / 2;
+
+        // Create current node
+        TreeNode root = new TreeNode(nums[mid]);
+
+
+        root.left = sortedArrayToBSTHelper(nums, root, start, mid - 1);
+        root.right = sortedArrayToBSTHelper(nums, root, mid + 1, end);
+
+
+        return root;
+    }
 }
