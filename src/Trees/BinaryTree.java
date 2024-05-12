61b6be5b4599aa911fb4136b02d81ef3638e3623
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-12 19:22:56 +0530

    Leetcode 107

--- src/Trees/BinaryTree.java
+++ src/Trees/BinaryTree.java
@@ -367,4 +367,30 @@ public class BinaryTree {
 
         return completeAns;
     }
+
+    //Leetcode 107
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null)
+            return new ArrayList<>();
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+                list.add(node.val);
+                if (node.left != null)
+                    q.add(node.left);
+                if (node.right != null)
+                    q.add(node.right);
+            }
+            result.add(0, list);
+        }
+        return result;
+    }
 }
