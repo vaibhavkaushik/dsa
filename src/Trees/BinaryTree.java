db56f855ab1aaa2a543be2a80e732b87f45f4493
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-12 17:46:57 +0530

    Leetcode 116

--- src/Trees/BinaryTree.java
+++ src/Trees/BinaryTree.java
@@ -15,6 +15,8 @@ public class BinaryTree {
         TreeNode left;
         TreeNode right;
 
+        TreeNode next;
+
         TreeNode (int val){
             this.val = val;
         }
@@ -288,5 +290,32 @@ public class BinaryTree {
             }
         }
         return queue.peek();
+    }
+
+    //Leetcode 116
+    public TreeNode connect(TreeNode root) {
+        if (root == null) return root;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                if (curr.left != null){
+                    q.add(curr.left);
+                }
+                if (curr.right != null) {
+                    q.add(curr.right);
+                }
+                if (i == size - 1){
+                    curr.next = null;
+                } else {
+                    curr.next = q.peek();
+                }
+            }
+        }
+        return root;
     }
 }
