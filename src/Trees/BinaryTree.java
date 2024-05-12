93d0d3b247392e06c89e22d48c5d2c6e8df04078
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-12 17:06:57 +0530

    Level order successor

--- src/Trees/BinaryTree.java
+++ src/Trees/BinaryTree.java
@@ -264,4 +264,29 @@ public class BinaryTree {
         return averageOfLevels;
 
     }
+
+    //Find level order successor of a given node
+    public TreeNode findSuccessor(TreeNode root, int key){
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            TreeNode currentNode = queue.poll();
+            if (currentNode.left != null) {
+                queue.offer(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                queue.offer(currentNode.right);
+            }
+            if (currentNode.val == key) {
+                break;
+            }
+        }
+        return queue.peek();
+    }
 }
