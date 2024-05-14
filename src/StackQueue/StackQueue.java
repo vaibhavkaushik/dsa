5c6ffb850e81609e970fd27fe2fdb072ed549583
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-14 23:38:42 +0530

    Leetcode 1021

--- src/StackQueue/StackQueue.java
+++ src/StackQueue/StackQueue.java
@@ -19,4 +19,32 @@ public class StackQueue {
         }
         return maxNumParentheses;
     }
+
+
+    /*
+This is a solution to the problem of removing outermost parentheses from a string containing only parentheses.
+
+The approach used is to keep track of the parentheses using a stack. Whenever an opening parenthesis is encountered, it is pushed onto the stack. Whenever a closing parenthesis is encountered, the last opening parenthesis is popped from the stack.
+
+If the stack size is greater than zero after pushing or popping, it means that the parenthesis is not an outer parenthesis, and it is added to the result string. If the stack size is zero, it means that the parenthesis is an outer parenthesis and it is not added to the result string.
+*/
+    //Leetcode 1021
+    public String removeOuterParentheses(String s) {
+        Stack<Character> bracket = new Stack<>();
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                if(bracket.size()>0){
+                    sb.append(s.charAt(i));
+                }
+                bracket.push(s.charAt(i));
+            }else{
+                bracket.pop();
+                if(bracket.size()>0){
+                    sb.append(s.charAt(i));
+                }
+            }
+        }
+        return sb.toString();
+    }
 }
