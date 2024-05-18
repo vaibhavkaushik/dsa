48e99756f3a48dd9c908e0649c7fcbc581a805ad
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-18 14:57:05 +0530

    Leetcode 946

--- src/StackQueue/StackQueue.java
+++ src/StackQueue/StackQueue.java
@@ -745,4 +745,24 @@ then make right subtree(recursively)
         return i==j && j==popped.length;
     }
 
+    //Leetcode 946
+    //Better Code
+    class Solution {
+        public boolean validateStackSequences(int[] pushed, int[] popped) {
+            Stack<Integer> st = new Stack<>(); // Create a stack
+
+            int j = 0; // Intialise one pointer pointing on popped array
+
+            for(int val : pushed){
+                st.push(val); // insert the values in stack
+                while(!st.isEmpty() && st.peek() == popped[j]){ // if st.peek() values equal to popped[j];
+                    st.pop(); // then pop out
+                    j++; // increment j
+                }
+            }
+            return st.isEmpty(); // check if stack is empty return true else false
+        }
+    }
+
+
 }
