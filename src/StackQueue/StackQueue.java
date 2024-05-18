d9ac56a4846bd095354aad7d46b83d81b7c74c73
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-18 11:20:05 +0530

    Leetcode 2487

--- src/StackQueue/StackQueue.java
+++ src/StackQueue/StackQueue.java
@@ -545,4 +545,27 @@ then make right subtree(recursively)
         return temp;
     }
 
+    //Leetcode 2487 (Takes 63ms) Monotonic stack approach
+    public ListNode removeNodesMono(ListNode head) {
+        ListNode cur = head;
+        Stack<ListNode> stack = new Stack<>();
+
+        while (cur != null) {
+            while (!stack.isEmpty() && stack.peek().val < cur.val) {
+                stack.pop();
+            }
+            stack.push(cur);
+            cur = cur.next;
+        }
+
+        ListNode nxt = null;
+        while (!stack.isEmpty()) {
+            cur = stack.pop();
+            cur.next = nxt;
+            nxt = cur;
+        }
+
+        return cur;
+    }
+
 }
