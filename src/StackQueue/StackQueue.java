12c17e6fcfd6004a11be3645198c04f957a36b6a
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-19 11:17:54 +0530

    Leetcode 225

--- src/StackQueue/StackQueue.java
+++ src/StackQueue/StackQueue.java
@@ -846,4 +846,43 @@ then make right subtree(recursively)
             return q.size();
         }
     }
+
+    //Leetcode 225
+    class MyStack
+    {
+        Queue<Integer> queue;
+
+        public MyStack()
+        {
+            this.queue=new LinkedList<Integer>();
+        }
+
+        // Push element x onto stack.
+        public void push(int x)
+        {
+            queue.add(x);
+            for(int i=0;i<queue.size()-1;i++)
+            {
+                queue.add(queue.poll());
+            }
+        }
+
+        // Removes the element on top of the stack.
+        public int pop()
+        {
+            return queue.poll();
+        }
+
+        // Get the top element.
+        public int top()
+        {
+            return queue.peek();
+        }
+
+        // Return whether the stack is empty.
+        public boolean empty()
+        {
+            return queue.isEmpty();
+        }
+    }
 }
