a4b291bf110706f71cbc80860b1d01bb7b07ab30
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-19 23:45:44 +0530

    Minor modify

---
+++ src/StackQueue/NestedInteger.java
@@ -0,0 +1,18 @@
+package StackQueue;
+
+
+import java.util.List;
+
+public class NestedInteger {
+    public boolean isInteger() {
+        return false;
+    }
+
+    public Integer getInteger() {
+        return 0;
+    }
+
+    public List<NestedInteger> getList() {
+        return null;
+    }
+}
