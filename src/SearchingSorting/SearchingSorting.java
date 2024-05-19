59945852366bac202c108d9c8c930c42126670d0
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-20 00:45:14 +0530

    Leetcode 35

--- src/SearchingSorting/SearchingSorting.java
+++ src/SearchingSorting/SearchingSorting.java
@@ -23,4 +23,35 @@ public class SearchingSorting {
             }
             return max;
         }
+
+    //Leetcode 35
+    public int searchInsert(int[] nums, int target) {
+
+        int left = 0;
+        int found = 0;
+        int right = nums.length-1;
+
+
+        while(left <= right) {
+
+            int mid = left + (right-left)/2;
+
+            if(nums[mid]==target) {
+                return mid;
+            }
+
+            if(target>nums[mid]) {
+                left=mid+1;
+            }
+
+            if(target<nums[mid]) {
+                right=mid-1;
+            }
+
+
+        }
+
+        return left;
+
+    }
 }
