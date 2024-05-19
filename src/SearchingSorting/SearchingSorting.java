1d203b340ad1bc48a3fd7c1782d705b162b2b541
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-19 23:49:05 +0530

    Leetcode 1672

--- src/SearchingSorting/SearchingSorting.java
+++ src/SearchingSorting/SearchingSorting.java
@@ -11,4 +11,16 @@ public class SearchingSorting {
         }
         return count;
     }
+
+    //Leetcode 1672
+        public int maximumWealth(int[][] accounts) {
+            int max = 0;
+            for (int[] customer : accounts) {
+                int wealth = 0;
+                for (int account : customer)
+                    wealth += account;
+                max = Math.max(max, wealth);
+            }
+            return max;
+        }
 }
