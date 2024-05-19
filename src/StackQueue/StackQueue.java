27bbe50b6cc649c04b1c6e5ca9a67785a55555ec
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-19 17:38:48 +0530

    Leetcode 2444

--- src/StackQueue/StackQueue.java
+++ src/StackQueue/StackQueue.java
@@ -1001,4 +1001,29 @@ In our implementation we will implement it in a way that we pre-calculate how mu
             return seconds;
         }
 
+    //https://www.youtube.com/watch?v=Bk-HxzaooqM
+    //Leetcode 2444
+        public long countSubarrays(int[] nums, int minK, int maxK) {
+            long res = 0;
+            int badIdx = -1, leftIdx = -1, rightIdx = -1;
+
+            for (int i = 0; i < nums.length; ++i) {
+                if (!(minK <= nums[i] && nums[i] <= maxK)) {
+                    badIdx = i;
+                }
+
+                if (nums[i] == minK) {
+                    leftIdx = i;
+                }
+
+                if (nums[i] == maxK) {
+                    rightIdx = i;
+                }
+
+                res += Math.max(0, Math.min(leftIdx, rightIdx) - badIdx);
+            }
+
+            return res;
+        }
+
 }
