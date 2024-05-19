286333af3a7ca5430965a87b0b66e236346da0cb
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-19 12:13:07 +0530

    Leetcode 950

--- src/StackQueue/StackQueue.java
+++ src/StackQueue/StackQueue.java
@@ -885,4 +885,59 @@ then make right subtree(recursively)
             return queue.isEmpty();
         }
     }
+
+
+    //Leetcode 950 (https://www.youtube.com/watch?v=S6MGVmkMz2c)
+    public int[] deckRevealedIncreasing(int[] deck) {
+        int n = deck.length;
+        int[] result = new int[n];
+
+        boolean skip = false;
+
+        int i = 0; // deck
+        int j = 0; // result
+
+        Arrays.sort(deck);
+
+        while (i < n) {
+            if (result[j] == 0) { // khali hai
+                if (!skip) {
+                    result[j] = deck[i];
+                    i++;
+                }
+
+                skip = !skip; // alternate
+            }
+
+            j = (j + 1) % n;
+        }
+
+        return result;
+    }
+
+    //Leetcode 950 (https://www.youtube.com/watch?v=S6MGVmkMz2c)
+    public int[] deckRevealedIncreasingQueueSimulation(int[] deck) {
+        int n = deck.length;
+
+        Queue<Integer> que = new LinkedList<>();
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            que.offer(i);
+        }
+
+        Arrays.sort(deck);
+
+        for (int i = 0; i < n; i++) {
+            int idx = que.poll();
+            result[idx] = deck[i];
+
+            if (!que.isEmpty()) {
+                que.offer(que.peek());
+                que.poll();
+            }
+        }
+
+        return result;
+    }
 }
