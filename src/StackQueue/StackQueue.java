2008ee3f3031715f4cbf304996048747161d937f
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-19 13:27:04 +0530

    Leetcode 2073

--- src/StackQueue/StackQueue.java
+++ src/StackQueue/StackQueue.java
@@ -963,4 +963,21 @@ then make right subtree(recursively)
         return queue.poll();
 
     }
+
+    //Leetcode 2073
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int count = 0;
+        while(tickets[k]!=0){
+            for(int i=0;i<tickets.length;i++){
+                if(tickets[i]!=0){
+                    tickets[i]-=1;
+                    count++;
+                }
+                if(tickets[k]==0){
+                    break;
+                }
+            }
+        }
+        return count;
+    }
 }
