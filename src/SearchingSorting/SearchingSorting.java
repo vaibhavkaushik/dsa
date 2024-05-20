49205dcdeac4010595d9828043dede5fb9022904
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-20 19:52:23 +0530

    Leetcode 786

--- src/SearchingSorting/SearchingSorting.java
+++ src/SearchingSorting/SearchingSorting.java
@@ -361,4 +361,24 @@ means we're at the right side and the ones greater than current element must hav
             return false;
         }
 
+        //Leetcode 786 (Unoptimized)
+        public int[] kthSmallestPrimeFraction(int[] arr, int k) {
+            PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
+
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = i + 1; j < arr.length; j++) {
+                    double div = (double) arr[i] / arr[j];
+                    pq.offer(new double[]{div, (double) arr[i], (double) arr[j]});
+                    if (pq.size() > k)
+                        pq.poll();
+                }
+            }
+
+            double[] vec = pq.peek();
+            int[] result = new int[2];
+            result[0] = (int) vec[1];
+            result[1] = (int) vec[2];
+            return result;
+        }
+
 }
