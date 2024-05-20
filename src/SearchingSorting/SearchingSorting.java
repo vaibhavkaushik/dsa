4b8e3bfb1721f7a9c3066bdb75ffe73eb634294b
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-20 12:21:38 +0530

    Leetcode 2529

--- src/SearchingSorting/SearchingSorting.java
+++ src/SearchingSorting/SearchingSorting.java
@@ -214,4 +214,34 @@ Priority queue se k weakest rows nikaal kar unke indices output array mein store
         return ans;
 
     }
+
+    //Leetcode 2529
+    public int maximumCount(int[] nums) {
+        int start = 0, end = nums.length-1;
+        int pos = 0, neg = 0;
+        //Use binary search to find positive number
+        while (start <= end) {
+            int mid = (start + end)/2;
+            if (nums[mid] > 0) {
+                pos = nums.length - mid;
+                end = mid - 1;
+            }
+            if (nums[mid] <= 0) {
+                start = mid + 1;
+            }
+        }
+        //Use binary search to find negative number
+        int first = 0, last = nums.length-1;
+        while (first <= last) {
+            int mid = (first + last)/2;
+            if (nums[mid] < 0) {
+                neg = mid + 1;
+                first = mid + 1;
+            }
+            if (nums[mid] >= 0) {
+                last = mid - 1;
+            }
+        }
+        return Math.max(pos,neg);
+    }
 }
