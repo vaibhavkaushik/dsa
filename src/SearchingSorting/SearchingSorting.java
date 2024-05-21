47b364041b11a9a28938a8f3d0edb96ca413bd0c
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-22 00:03:17 +0530

    Leetcode 81

--- src/SearchingSorting/SearchingSorting.java
+++ src/SearchingSorting/SearchingSorting.java
@@ -875,6 +875,7 @@ means we're at the right side and the ones greater than current element must hav
     }
 
     //Leetcode 33
+    //https://www.youtube.com/watch?v=U1VsdRgVevY
     public int search(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
             return -1;
@@ -919,6 +920,46 @@ means we're at the right side and the ones greater than current element must hav
             }
         }
         return -1;
+    }
+
+    //Leetcode 81
+    public boolean searchII(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            while (start < mid && nums[start] == nums[mid] && nums[end] == nums[mid]) {
+                start++;
+                end--;
+            }
+
+            if (nums[start] <= nums[mid]) {
+                // Left side is sorted. Right side is unsorted.
+                if (target >= nums[start] && target < nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                // Left side is unsorted. Right side is sorted.
+                if (target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return false;
     }
 
 }
