a64ef05508688ca48eea0279261f1c42a65c75e8
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-24 23:29:12 +0530

    Leetcode 875

--- src/SearchingSorting/SearchingSorting.java
+++ src/SearchingSorting/SearchingSorting.java
@@ -1307,4 +1307,26 @@ I hope yeh detailed explanation aapko samajh mein aaya hoga. Agar aur koi doubts
         return ans;
     }
 
+    //Leetcode 875
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = Integer.MAX_VALUE;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(canEatInTime(piles, mid, h)) right = mid - 1;
+            else left = mid + 1;
+        }
+        return left;
+    }
+    public boolean canEatInTime(int piles[], int k, int h){
+        int hours = 0;
+        for(int pile : piles){
+            int div = pile / k;
+            hours += div;
+            if(pile % k != 0) hours++;
+        }
+        return hours <= h;
+    }
+
 }
