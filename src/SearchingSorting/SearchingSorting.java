45155e63cab36f90a2d7a2b780dead4460384427
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-26 13:52:45 +0530

    Leetcode 2448

--- src/SearchingSorting/SearchingSorting.java
+++ src/SearchingSorting/SearchingSorting.java
@@ -1404,4 +1404,36 @@ I hope yeh detailed explanation aapko samajh mein aaya hoga. Agar aur koi doubts
             return result;
         }
 
+        //Leetcode 2448
+        // Method to calculate the cost given a target
+        public long getCost(int[] nums, int[] cost, int target) {
+            long result = 0;
+            for (int i = 0; i < nums.length; ++i) {
+                result += (long) Math.abs(nums[i] - target) * cost[i];
+            }
+            return result;
+        }
+
+    // Method to find the minimum cost
+    public long minCost(int[] nums, int[] cost) {
+        long answer = Long.MAX_VALUE;
+
+        int left = Arrays.stream(nums).min().orElse(0);  //1
+        int right = Arrays.stream(nums).max().orElse(0); //5
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            long cost1 = getCost(nums, cost, mid);
+            long cost2 = getCost(nums, cost, mid + 1);
+
+            answer = Math.min(cost1, cost2);
+            if (cost1 > cost2)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return answer == Long.MAX_VALUE ? 0 : answer;
+    }
+
 }
