f3c51080c557f8ed8e699090703cde36367f8808
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-05-27 21:34:55 +0530

    Leetcode 2760

--- src/SlidingWindow/SlidingWindow.java
+++ src/SlidingWindow/SlidingWindow.java
@@ -608,6 +608,28 @@ public class SlidingWindow {
         return minLength == Integer.MAX_VALUE ? -1 : minLength;
     }
 
+    //Leetcode 2760
+    public int longestAlternatingSubarray(int[] nums, int threshold) {
+        int answer = 0, i = 1, r = Integer.MAX_VALUE;
+        if(nums[0] <= threshold && nums[0] % 2 == 0) {
+            r = 0;
+        }
+        while(i < nums.length){
+            if( nums[i] > threshold || ((nums[i] % 2) == (nums[i-1] % 2)) ){
+                // Conditions not satisfied
+                answer = Math.max(answer, i - r );
+                r = Integer.MAX_VALUE;
+            }
+            if(r == Integer.MAX_VALUE && nums[i] <= threshold && nums[i] % 2 == 0){
+                // Conditions satisfied - increase the length of subarray
+                r = i;
+            }
+            i++;
+        }
+        answer = Math.max(answer, i - r );
+        return answer;
+    }
+
 
     public static void main(String[] args) {
         long[] A = { -8, 2, 3, -6, 10 };
