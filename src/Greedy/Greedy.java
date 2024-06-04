08dd99f0757c8520846a70b5f48f215af97ee860
VAIBHAV KAUSHIK <vaibhavkaushik23@gmail.com>
2024-06-04 22:33:21 +0530

    Leetcode 1974

--- src/Greedy/Greedy.java
+++ src/Greedy/Greedy.java
@@ -290,5 +290,19 @@ public class Greedy {
         return ans; // Final permutation array return karte hain
     }
 
+        //Leetcode 1974
+        public int minTimeToType(String word) {
+        int n=word.length();
+        int sum=0;
+        int start=(int)'a';
+        for(int i=0;i<n;i++){
+            int curr=(int)word.charAt(i);
+            int x=Math.min(Math.abs(start-curr),26-Math.abs(start-curr));
+            sum=sum+x;
+            start=curr;
+        }
+        return sum+n;
+    }
+
 
 }
