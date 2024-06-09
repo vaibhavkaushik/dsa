148ab8c1a4d9251f66c50b785a9a683eda6db9f3
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-06-09 21:05:40 +0530

    Leetcode 1442

--- src/BitManipulation/BitManipulation.java
+++ src/BitManipulation/BitManipulation.java
@@ -246,4 +246,28 @@ public class BitManipulation {
         }
         return bitCount;
     }
+
+    //Leetcode 1442
+    public int countTriplets(int[] arr) {
+        int n = arr.length;
+        int[] prefixXor = new int[n + 1];
+
+        // Prefix XOR array banaye
+        for (int i = 0; i < n; i++) {
+            prefixXor[i + 1] = prefixXor[i] ^ arr[i];
+        }
+
+        int count = 0;
+
+        // Har pair (i, k) ke liye check karein
+        for (int i = 0; i < n; i++) {
+            for (int k = i + 1; k < n; k++) {
+                if (prefixXor[i] == prefixXor[k + 1]) {
+                    count += (k - i);
+                }
+            }
+        }
+
+        return count;
+    }
 }
