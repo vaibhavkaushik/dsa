eee517752656ba9d5923c573b1ee68c3e9adfb1a
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-06-18 19:56:30 +0530

    Leetcode 133

--- src/Graph/GraphQuestions.java
+++ src/Graph/GraphQuestions.java
@@ -724,5 +724,49 @@ public class GraphQuestions {
         return champion;
     }
 
+    //Leetcode 133
+
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+    private Node dfs(Node node, Map<Node, Node> map) {
+        if (node == null) return null;
+
+        // If the node is already cloned, return the cloned node
+        if (map.containsKey(node)) {
+            return map.get(node);
+        }
+
+        // Clone the node
+
+        Node cloneNode = new Node(node.val);
+        map.put(node, cloneNode);
+
+        // Clone all the neighbors
+        for (Node neighbor : node.neighbors) {
+            cloneNode.neighbors.add(dfs(neighbor, map));
+        }
+
+        return cloneNode;
+    }
+
+    public Node cloneGraph(Node node) {
+        Map<Node, Node> map = new HashMap<>();
+        return dfs(node, map);
+    }
 
 }
