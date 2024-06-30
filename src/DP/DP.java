652cbd027873be6f24cd341730a8c0c797647fbf
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-06-30 20:53:11 +0530

    Leetcode 509

---
+++ src/DP/DP.java
@@ -0,0 +1,38 @@
+package DP;
+
+public class DP {
+
+    //Leetcode 509
+    public int fibTopDown(int n) {
+        int[] dp = new int[n+1];
+        return fibHelper(n,dp);
+    }
+
+    public int fibHelper(int n, int[] dp){
+        if(n <= 1){
+            dp[n] = n;
+            return n;
+        }
+
+        dp[n] = fibHelper(n-1,dp) + fibHelper(n-2,dp);
+
+        return dp[n];
+    }
+
+    //Leetcode 509
+    public int fib(int n) {
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+
+        if(n>=1){
+            dp[1] = 1;
+        }
+
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+    }
+}
