9a45777ae65d579d7125248290fd483831a70d72
Vaibhav Kaushik <vaibhavkaushik23@gmail.com>
2024-07-01 22:16:30 +0530

    Leetcode 538 Morris

--- src/TopQuestions/Leetcode538.java
+++ src/TopQuestions/Leetcode538.java
@@ -22,4 +22,34 @@ public class Leetcode538 {
 
         }
     }
+
+    class SolutionMorris {
+        public TreeNode convertBST(TreeNode root) {
+            TreeNode cur= root;
+            int sum = 0;
+            while (cur != null) {
+                if (cur.right == null) {
+                    int tmp = cur.val;
+                    cur.val += sum;
+                    sum += tmp;
+                    cur = cur.left;
+                } else {
+                    TreeNode prev = cur.right;
+                    while (prev.left != null && prev.left != cur)
+                        prev = prev.left;
+                    if (prev.left == null) {
+                        prev.left = cur;
+                        cur = cur.right;
+                    } else {
+                        prev.left = null;
+                        int tmp = cur.val;
+                        cur.val += sum;
+                        sum += tmp;
+                        cur = cur.left;
+                    }
+                }
+            }
+            return root;
+        }
+    }
 }
