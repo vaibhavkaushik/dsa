bfffa10fc832815b6d71c51495e968b4094098a7
VAIBHAV KAUSHIK <vaibhavkaushik23@gmail.com>
2024-07-02 12:20:09 +0530

    Update Leetcode995.java

--- src/TopQuestions/Leetcode995.java
+++ src/TopQuestions/Leetcode995.java
@@ -1,27 +1,30 @@
 package TopQuestions;
 
 public class Leetcode995 {
-class Solution {
-    public int minKBitFlips(int[] nums, int k) {
-        int count = 0;
-        int flip = 0;
-        int[] isFlipped = new int[nums.length];
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (i >= k) {
-                flip ^= isFlipped[i - k];
-            }
-            if ((nums[i] == 0 && flip % 2 == 0) || (nums[i] == 1 && flip % 2 == 1)) {
-                if (i + k > nums.length) {
-                    return -1;
+    class Solution {
+            public int minKBitFlips(int[] nums, int k) {
+            int count = 0;
+            int flipCount = 0;
+            int[] isFlipped = new int[nums.length];
+            
+            for (int i = 0; i < nums.length; i++) {
+                //i>=k ka mtlb hua ki window exceed karli, and agar window exceed krli hai toh iska mtlb ye hai ki flipCount   
+                //change karna padega, kyoonki ab jo new window start hoyegi, uspe k index pehle ka fark nahi padna chahiye
+                //Ab agar flipCount 1 hai to 0 banao, 0 hai toh 1 banao
+                if (i >= k) {
+                    flipCount ^= isFlipped[i - k];
                 }
-                flip += 1;
-                isFlipped[i] = 1;
-                count++;
+                if ((nums[i] == 0 && flipCount % 2 == 0) || (nums[i] == 1 && flipCount % 2 == 1)) {
+                    if (i + k > nums.length) {
+                        return -1;
+                    }
+                    flipCount += 1;
+                    isFlipped[i] = 1;
+                    count++;
+                }
             }
+            
+            return count;
         }
-        
-        return count;
     }
-}
 }
