5b31d070337ce0c428811531e54d38cdba476c1b
VAIBHAV KAUSHIK <vaibhavkaushik23@gmail.com>
2024-07-02 11:33:22 +0530

    Create Leetcode995.java

---
+++ src/TopQuestions/Leetcode995.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minKBitFlips(int[] nums, int k) {
+        int count = 0;
+        int flip = 0;
+        int[] isFlipped = new int[nums.length];
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i >= k) {
+                flip ^= isFlipped[i - k];
+            }
+            if ((nums[i] == 0 && flip % 2 == 0) || (nums[i] == 1 && flip % 2 == 1)) {
+                if (i + k > nums.length) {
+                    return -1;
+                }
+                flip += 1;
+                isFlipped[i] = 1;
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
